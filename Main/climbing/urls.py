1301fd9d78fbea523a6d84d548d441734451d73a
ksanbal <dev.ksanbal@gmail.com>
2020-06-23 02:05:28 +0900

    각 페이지들 주석 달고 url 작성

--- Main/climbing/urls.py
+++ Main/climbing/urls.py
@@ -2,6 +2,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Auth
     path('', views.signin, name='signin'),
     path('signup/', views.signup, name='signup'),
+
+    # Main
+    path('main/', views.main, name='main'),
+
+    # list
+    path('list/', views.postlist_main, name='listmain'),
+    path('list/detail/', views.postlist_detail, name='listdetail'),
+    path('list/post/', views.postlist_post, name='listpost'),
+
+    # Record
+
+    # My list
+
+    # Friend
+
+    # etc
+    path('error/', views.errorpage, name='error'),
 ]
