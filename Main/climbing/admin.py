e2a5f2522bab143530c7b66d1d8649ee0ca46fec
ksanbal <dev.ksanbal@gmail.com>
2020-06-22 18:22:26 +0900

    admin 페이지에서 모든 모델에 접근할 수 있도록 작성

---
+++ Main/climbing/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Account)
+admin.site.register(Mountains)
+admin.site.register(PostMountain)
+admin.site.register(MyList)
+
+# Register your models here.
