fe0a2d29bdba3befd40c8ed770c84d189a2b8297
ksanbal <dev.ksanbal@gmail.com>
2020-06-23 02:04:48 +0900

    List Post를 위한 Form 작성

--- Main/climbing/forms.py
+++ Main/climbing/forms.py
@@ -12,3 +12,10 @@ class SigninForm(forms.ModelForm):
     class Meta:
         model = Account
         fields = ['username', 'pw']
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = PostMountain
+        fields = ['imgpath', 'name', 'body', 'star']
+
