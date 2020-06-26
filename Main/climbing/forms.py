0e75e89ca9b127ac83a3b33519aaf7c4aedb8104
ksanbal <dev.ksanbal@gmail.com>
2020-06-26 18:20:02 +0900

    등산기록을 남기는 Record Form 작성

--- Main/climbing/forms.py
+++ Main/climbing/forms.py
@@ -19,3 +19,21 @@ class PostForm(forms.ModelForm):
         model = PostMountain
         fields = ['imgpath', 'name', 'body', 'star']
 
+
+class RecordForm(forms.ModelForm):
+    class Meta:
+        model = MyList
+        fields = ['m_name', 'course', 'time']
+        widgets = {
+            'm_name' : forms.TextInput(
+                attrs={'class':'form-input1', 'placeholder': '산 이름'}
+            ),
+            'course': forms.TextInput(
+                attrs={'class': 'form-input1', 'placeholder': '코스 이름'}
+            ),
+            'time': forms.TextInput(
+                attrs={'class': 'form-input1', 'placeholder': '소요 시간'}
+            ),
+        }
+
+
