4836ccd06db28d127b060d60734fbaacd3e75f47
ksanbal <dev.ksanbal@gmail.com>
2020-06-26 18:20:40 +0900

    각종 페이지 링크 작업

--- Main/climbing/urls.py
+++ Main/climbing/urls.py
@@ -10,15 +10,20 @@ urlpatterns = [
     path('main/', views.main, name='main'),
 
     # list
-    path('list/', views.postlist_main, name='listmain'),
-    path('list/detail/', views.postlist_detail, name='listdetail'),
-    path('list/post/', views.postlist_post, name='listpost'),
+    path('list/', views.postlist_main, name='list_main'),
+    path('list/detail/<int:list_id>/', views.postlist_detail, name='list_detail'),
+    path('list/post/', views.postlist_post, name='list_post'),
 
     # Record
+    path('record/', views.record, name='record'),
 
     # My list
+    path('mylist/', views.mylist_main, name='mylist_main'),
+    path('mylist/<int:mylist_id>/', views.mylist_detail, name='mylist_detail'),
 
     # Friend
+    path('friend/', views.friend_main, name='friend_main'),
+    path('friend/<int:friend_id>/', views.friend_detail, name='friend_detail'),
 
     # etc
     path('error/', views.errorpage, name='error'),
